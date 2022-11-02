396cd2f2a4d3fd2779398a4a8c51f5315a7971dc
Tuski2304 <otuchituru.anyahara@gmail.com>
2022-11-02 04:11:04 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,22 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *print_diagsums - prints the sum of the two
- *diagonals of a square matrix of integers
- *@a: input pointer.
- *@size: size of the matrix
- *Return: no return.
-*/
-void print_diagsums(int *a, int size)
+ * print_chessboard - prints the chessboard
+ * @a: input pointer.
+ * Return: no return.
+ */
+void print_chessboard(char (*a)[8])
 {
-int i, sum1 = 0, sum2 = 0;
-for (i = 0; i < (size * size); i++)
-{
-if (i % (size + 1) == 0)
-sum1 += *(a + i);
-if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-sum2 += *(a + i);
-}
-printf("%d, %d\n", sum1, sum2);
+	unsigned int i, m = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
+	}
+	_putchar('\n');
 }
