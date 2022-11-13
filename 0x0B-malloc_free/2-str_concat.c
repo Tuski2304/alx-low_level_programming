cf7bfaff8897c57b1ac334ea7e2bd3fb1d5f05f4
Tuski2304 <otuchituru.anyahara@gmail.com>
2022-11-13 06:34:19 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 /**
- *alloc_grid - returns a pointer to a 2 dimensional array of integers.
- *@width: width of the array.
- *@height: height of the array.
- *Return: pointer of an array of integers
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
+ *
+ * Return: pointer of an array of integers
 */
 int **alloc_grid(int width, int height)
 {
